ae6ad9e479c5684adaebe8b95204590b20ddf145
tejaswiniR161 <tejaswinigowda1996@gmail.com>
2020-02-13 00:21:24 -0500

    Had missed this file before

--- p5.m
+++ p5.m
@@ -24,7 +24,13 @@ yset2 = stddev.*randn(100000,1) + mean;
    %for j = 1:length(set2)
     %    set2(j)=min(-3,max(3,set2(j)));
    %end
-    
+   %x = linspace(0,1000);
+    y = [0,20];
+
+   for j = 1:length(set1)
+       x = set1(j);
+        line(x,y)
+   end
 
 hold on;
 %h1=histfit(set1);
@@ -35,12 +41,12 @@ hold on;
 %set(h2(1),'facecolor',[0.2,0.5,0.5]);
 %set(h2(1),'FaceAlpha',0.5);
 
-h3=histfit(set2);
-set(h3(1),'facecolor',[0.5,0.5,0.5]);
-set(h3(1),'FaceAlpha',0.5);
+%h3=histfit(set2);
+%set(h3(1),'facecolor',[0.5,0.5,0.5]);
+%set(h3(1),'FaceAlpha',0.5);
 
-h4=histfit(yset2);
-set(h4(1),'facecolor',[0.7,0.7,0.5]);
-set(h4(1),'FaceAlpha',0.5);
+%h4=histfit(yset2);
+%set(h4(1),'facecolor',[0.7,0.7,0.5]);
+%set(h4(1),'FaceAlpha',0.5);
 
 hold off
